20ba972f2bf80b41597104d413a668c240cb4ae5
LucasKane29 <lucaskane4@gmail.com>
2026-02-16 21:23:57 +0200

    Lesson #10
    Finished homework from Lesson #10

---
+++ Assets/Lecture10/Scripts/MeshCombiner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MeshCombiner : MonoBehaviour
+{
+    [SerializeField] private MeshFilter[] _meshFilters;
+
+    private Mesh _mesh;
+
+    [ContextMenu("Combine Meshes")]
+    private void CombineMeshes()
+    {
+        _mesh = new Mesh();
+
+        Matrix4x4 objectMatrix = transform.worldToLocalMatrix;
+        CombineInstance[] instances = new CombineInstance[_meshFilters.Length];
+
+        for (int i = 0; i < _meshFilters.Length; ++i)
+        {
+            CombineInstance instance = new CombineInstance();
+            instance.mesh = _meshFilters[i].sharedMesh;
+            instance.transform = objectMatrix * _meshFilters[i].transform.localToWorldMatrix;
+            instances[i] = instance;
+        }
+
+        _mesh.CombineMeshes(instances, false, true);
+
+#if UNITY_EDITOR
+        string path = EditorUtility.SaveFilePanelInProject("Save Mesh", "New Mesh", "asset", "");
+        AssetDatabase.CreateAsset(_mesh, path);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+#endif
+    }
+}
